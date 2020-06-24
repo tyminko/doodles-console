6b7fde8cda37812f3fa7ff48e83dae54709bec98
maxim <tyminko@gmail.com>
2020-06-24 21:39:41 +0200

    v1

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,408 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" :style="mainFontSizeStyle">
+    <div v-for="(line, j) in lines" :key="`dh${j}`"  class="line" v-html="line" />
+<!--    <div class="line"><span class="box">###</span></div>-->
+<!--    <div v-for="(drawing, n) in drawings" :key="`dr${n}`">-->
+<!--      <div v-for="(line, i) in drawing.header" :key="`drh${i}`"  class="line" v-html="line" />-->
+<!--      <div v-for="(doodle, i) in drawing.doodles" :key="`d${i}`" class="doodle">-->
+<!--        <div v-for="(line, j) in doodle.header" :key="`dh${j}`"  class="line" v-html="line" />-->
+<!--        <div v-for="(line, j) in doodle.state" :key="`ds${j}`"  class="line" v-html="line" />-->
+<!--        <div class="line"/>-->
+<!--        <div class="line"><span class="box">##</span></div>-->
+<!--        <div class="line"/>-->
+<!--      </div>-->
+<!--    </div>-->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import * as db from './lib/db'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  },
+  data: () => ({
+    uid: null,
+    fontSize: 18,
+    maxSegments: 5,
+    drawings: {},
+    currentDrawingDate: null,
+    currentDrawingNr: null,
+    currentDrawingWidth: null,
+    numRows: null,
+    numCharInLine: null,
+    linePointers: []
+  }),
+  created () {
+    db.auth(user => {
+      const uid = (user || {}).uid || null
+      if (uid) {
+        db.subscribeSettings(uid, this.updateSettings)
+        db.subscribeDrawing(uid, drawingData => {
+          this.updateDrawingData(drawingData)
+          this.updateLines()
+        })
+        db.subscribeState(uid, this.updateStateData)
+      }
+    })
+  },
+
+  computed: {
+    mainFontSizeStyle () {
+      return { fontSize: `${this.fontSize}px` }
+    },
+    lines () {
+      return this.linePointers.map(pointer => {
+        return pointer.reduce((res, key) => {
+          return res[key]
+        }, this.drawings)
+      })
+    }
+  },
+
+  methods: {
+    async updateLines () {
+      this.lines.push('')
+      if (!this.currentDrawingNr) return
+      const dNr = this.currentDrawingNr
+      const dr = this.drawings[this.currentDrawingNr]
+      await this.printBlock(dr.header, [dNr, 'header'])
+      const promise = dr.doodles.reduce((promiseChain, doodle, i) => {
+        return promiseChain.then(() => {
+          return this.printBlock(doodle.header, [dNr, 'doodles', i, 'header'])
+            .then(() => this.printBlock(doodle.state, [dNr, 'doodles', i, 'state']))
+        })
+      }, Promise.resolve())
+      promise.then(() => console.log('PRINT READY'))
+      // dr.doodles.forEach(doodle => {
+      //   this.printBlock(doodle.header)
+      // })
+    },
+
+    async printBlock (block, address, time = 0) {
+      return block.reduce((promiseChain, item, i) => {
+        return promiseChain.then(() => {
+          this.linePointers.push([...address, i])
+          return new Promise(resolve => {
+            setTimeout(() => {
+              resolve()
+            }, time)
+          })
+        })
+      }, Promise.resolve())
+    },
+
+    /**
+     * @param {Settings} settings
+     */
+    async updateSettings (settings) {
+      if (!settings) return
+      this.fontSize = settings.fontSize || 10
+      this.maxSegments = settings.maxSegments || 5
+      await this.$nextTick(() => {
+        this.numRows = window.innerHeight / this.fontSize
+      })
+    },
+
+    /**
+     * @param {DrawingData} drawing
+     */
+    updateDrawingData (drawing) {
+      this.currentDrawingNr = drawing.number
+      this.currentDrawingWidth = drawing.width
+      this.currentDrawingDate = drawing
+      this.$set(this.drawings, drawing.number, {
+        header: this.drawingHeaderStr(drawing).split('\n'),
+        doodles: drawing.doodles.map((doodle, i) => ({
+          header: this.doodleHeaderStr(doodle, i, drawing.number, drawing.width).split('\n'),
+          state: []
+        }))
+      })
+    },
+
+    /**
+     * @param {DrawingState} state
+     * @param {number} drW
+     */
+    updateStateData (state, drW) {
+      if (!this.currentDrawingNr || !this.drawings[this.currentDrawingNr]) return
+      this.drawings[this.currentDrawingNr].doodles.forEach((doodle, i) => {
+        const dd = this.currentDrawingDate.doodles[i]
+        const drawable = [...(dd.drawingPoints || []), (dd.segments || []).length - 1]
+        this.$set(this.drawings[this.currentDrawingNr].doodles[i], 'state', this.doodleStateStr(state[i], this.currentDrawingWidth, drawable))
+      })
+    },
+
+    /**
+     * @param {SegmentState[]} state
+     * @param {number} drW
+     * @param {number[]} drawable
+     * @return {string[]}
+     */
+    doodleStateStr (state, drW = 100, drawable) {
+      let l1 = ''
+      let l2 = ''
+      let l3 = ''
+      state.forEach((segState, i) => {
+        const cl = drawable.includes(i - 1) ? 'select' : 'box'
+        l1 += `<span class="${cl}"><span class="spin dimmed">${this.space(2)}</span>` + this.number(segState.spin, 7) + '°' + this.space(2) + '</span>'
+        l2 += `<span class="${cl}"><span class="dimmed">x:</span>` + this.number(segState.x / drW * 100, 7) + this.space(3) + '</span>'
+        l3 += `<span class="${cl}"><span class="dimmed">y:</span>` + this.number(segState.y / drW * 100, 7) + this.space(3) + '</span>'
+        if (segState.xEnd && segState.yEnd) {
+          l2 += `<span class="select"><span class="dimmed">x:</span>` + this.number(segState.xEnd / drW * 100, 7) + this.space(3) + '</span>'
+          l3 += `<span class="select"><span class="dimmed">y:</span>` + this.number(segState.yEnd / drW * 100, 7) + this.space(3) + '</span>'
+        }
+      })
+      return [l1, l2, l3]
+    },
+
+    /**
+     * @param {DoodleData} doodle
+     * @param {string} index
+     * @param {number} drNum
+     * @param {number} drW
+     * @return {string}
+     */
+    doodleHeaderStr (doodle, index, drNum, drW) {
+      /*
+       * 1: Doodle #12549-1 (top-left)
+       * Virtual drawing devise: A chain of 3 spinning line-segments with a fixed origin and
+       * 2 drawing points on the vertices of the 2nd and 5th segments.
+       * Probability of a spin flip for each segment less than 10%
+       *
+      * */
+      const doodN = `${index + 1}`
+      const numSegments = doodle.segments.length
+      const numPoints = (doodle.drawingPoints || []).length + 1
+      const chance = Math.round(this.round(doodle.probabilitySpinFlip) * 100)
+      let text = `${doodN.padEnd(2, ' ')}: Doodle #${drNum}-${doodN}
+      <span class="str">Drawing devise</span>: A chain of <span class="num">${numSegments}</span> spinning line-segments with a fixed origin and <span class="num">${numPoints}</span> drawing points.
+      `
+      if (chance) {
+        text += `Probability of spin flip for each segment less than <span class="num">${chance}%</span>`
+      } else {
+        text += `With constant spin direction`
+      }
+      text += `
+      
+      `
+      /*
+       * A          B           A          D
+       * |⟵45.79⟶|⟵56.45⟶|⟵34.00⟶|
+       *                        ✐         ✐
+       */
+      const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+      doodle.segments.forEach((seg, i) => {
+        text += letters[i]
+        text += this.space(11)
+      })
+      text += `${letters[doodle.segments.length]}
+        `
+      doodle.segments.forEach((seg, i) => {
+        if (i === 0) text += '<span class="box">|</span>'
+        const l = this.number(seg.length / drW * 100, 5)
+        text += `<span class="box">───</span>${l}<span class="box">───|</span>`
+      })
+      text += `
+        `
+      text += '&nbsp;'
+      doodle.segments.forEach((seg, i) => {
+        if ((doodle.drawingPoints || []).includes(i) || i === doodle.segments.length - 1) {
+          text += this.space(11) + '<span class="pen str">&nbsp;</span>'
+        } else {
+          text += this.space(12)
+        }
+      })
+      return text
+    },
+
+    /**
+     * @param {DrawingData} drawing
+     * @return {string}
+     */
+    drawingHeaderStr (drawing) {
+      /**
+       * Drawing #12549, 4 Doodles (2x2):
+       */
+      let text = `
+      <span class="str">Drawing #${drawing.number}</span>, ${drawing.doodles.length} Doodles (${drawing.columns}x${drawing.rows}):
+      
+      `
+      /**
+       *  1 │ 2
+       * ───┼───
+       *  3 │ 4
+       */
+      for (let r = 0; r < drawing.rows; r++) {
+        if (r > 0) {
+          for (let c = 0; c < drawing.columns; c++) {
+            text += '<span class="box">'
+            if (c > 0) text += `┼`
+            text += `───`
+            if (drawing.doodles.length > 9) text += `─`
+          }
+          text += `</span>
+`
+        }
+        for (let c = 0; c < drawing.columns; c++) {
+          if (c > 0) text += `<span class="box">│</span>`
+          let nr = `${((c + 1) * (r + 1))}`
+          if (drawing.doodles.length > 9) nr = nr.padEnd(2, ' ')
+          text += `&nbsp;${nr}&nbsp;`
+        }
+        text += `
+`
+      }
+      return text
+    },
+
+    /**
+     * @param {number} n
+     * @param {string} str
+     * @return {string}
+     */
+    space (n, str = '') {
+      return [...Array(n)].map(_ => str || '&nbsp;').join('')
+    },
+
+    round (n) {
+      return Math.round(n * 100) / 100
+    },
+
+    number (n, chars) {
+      const intCars = chars - 3 // 2 decimals + dot
+      const rounded = '' + this.round(n)
+      const parts = rounded.split('.')
+      return (parts[0] || '0').padStart(intCars, '\xa0') + '.' + (parts[1] || '0').padEnd(2, '0')
+    }
+  },
+
+  beforeDestroy () {
+    db.unsubscribe()
   }
 }
+/**
+ * Drawing #12549, 4 Doodles (2x2):
+ *
+ *  1 │ 2
+ * ───┼───
+ *  3 │ 4
+ *
+ * 1: Doodle #12549-1 (top-left)
+ * Virtual drawing devise: A chain of 3 spinning line-segments with a fixed origin and
+ * 2 drawing points on the vertices of the 2nd and 5th segments.
+ * Probability of a spin flip for each segment less than 10%
+ *
+ * A          B           A          D
+ * |⟵45.79⟶|⟵56.45⟶|⟵34.00⟶|
+ *                        ✐         ✐
+ * ⤾ 0.2°     ⤿ -2.34°   ⤿ -0.34°  .
+ * x: 65.34   x: 12.34    x: 12.34   x:12.34
+ * y: 44.22   y:  5.22    y:  5.22   y: 5.22
+ *
+ */
+/**
+ * @typedef {{
+ *  fontSize: number,
+ *  maxSegments: number,
+ * }} Settings
+ *
+ * @typedef {{
+ *  spin: number,
+ *  x: number,
+ *  y: number
+ *  xEnd?: number,
+ *  yEnd?: number
+ * }} SegmentState
+ *
+ * @typedef {SegmentState[][]} DrawingState
+ *
+ * @typedef {{
+ *  length: number
+ * }} SegmentData
+ *
+ * @typedef {{
+ *  probabilitySpinFlip: number,
+ *  drawingPoints: number[],
+ *  segments: SegmentData[],
+ *  order: number
+ * }} DoodleData
+ *
+ * @typedef {{
+ *  number: number,
+ *  width: number,
+ *  height: number,
+ *  columns: number,
+ *  rows: number,
+ *  doodles: DoodleData[]
+ * }} DrawingData
+ *
+ */
 </script>
 
 <style lang="scss">
+  body {
+    margin: 0;
+    min-height: 100vh;
+    width: 100vw;
+    background: black;
+  }
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Roboto Mono', monospace;
+  font-size: 18px;
+  line-height: 1;
+  display: flex;
+  flex-flow: column;
+  justify-content: flex-end;
+  padding: 0 0 0 10px;
+  color: #fff;
+  background: black;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100vh;
+
+  .line {
+    min-height: 1em;
+
+    .select {
+      font-weight: 700;
+    }
+
+    .box {
+      color: #b5b5b5;
+    }
+    .dimmed {
+      color: #a58bc3;
+    }
+    .str {
+      color: #f296ff;
+    }
+    .num {
+      color: #00ffff;
+    }
+    .pen {
+      position: relative;
+      &:after {
+        content: '✐';
+        background-color: #fffa78;
+        padding: 0 2px;
+        color: black;
+        position: absolute;
+        left: 0;
+        top: 50%;
+        transform: translate(-2px, -50%);
+      }
+    }
+    .spin {
+      position: relative;
+      &:after {
+        content: '↻';
+        position: absolute;
+        left: 0;
+        top: 50%;
+        transform: translateY(-50%);
+      }
+    }
+  }
 }
 </style>
--- src/lib/db.js
+++ src/lib/db.js
@@ -1,143 +1,67 @@
-/**
- Drawing #12549, 4 Doodles (2x2):
+import firebase from 'firebase'
 
-  1 │ 2
- ───┼───
-  3 │ 4
+firebase.initializeApp({
+  apiKey: 'AIzaSyDHIJbOtxWHs-79n04xifJCvkuUEqbxvjQ',
+  authDomain: 'doodles-sync.firebaseapp.com',
+  databaseURL: 'https://doodles-sync.firebaseio.com',
+  projectId: 'doodles-sync',
+  storageBucket: 'doodles-sync.appspot.com',
+  messagingSenderId: '601337200849',
+  appId: '1:601337200849:web:bbc677a6cd5fa8200445f0'
+})
+const db = firebase.database()
+/** @type firebase.database.Reference */
+let settingsRef = null
+/** @type firebase.database.Reference */
+let drawingRef = null
+/** @type firebase.database.Reference */
+let stateRef = null
 
- 1: Doodle #12549-1 (top-left)
-    Virtual drawing devise: A chain of 3 spinning line-segments with a fixed origin and
-    2 drawing points on the vertices of the 2nd and 5th segments.
-    Probability of a spin flip for each segment less than 10%
-
-    A          B           A          D
-    |⟵45.79⟶|⟵56.45⟶|⟵34.00⟶|
-                           ✐         ✐
-    ⤾ 0.2°     ⤿ -2.34°   ⤿ -0.34°  .
-    x: 65.34   x: 12.34    x: 12.34   x:12.34
-    y: 44.22   y:  5.22    y:  5.22   y: 5.22
-
- */
-/**
- * @typedef {{
- *  spin: number,
- *  x: number,
- *  y: number
- * }[]} SegmentState
- *
- * @typedef {SegmentState[][]} DrawingState
- *
- * @typedef {{
- *  length: number
- * }} SegmentData
- *
- * @typedef {{
- *  probabilitySpinFlip: number,
- *  drawingPoints: number[],
- *  segments: SegmentData[],
- *  order: number
- * }} DoodleData
- *
- * @typedef {{
- *  number: number,
- *  width: number,
- *  height: number,
- *  columns: number,
- *  rows: number,
- *  doodles: DoodleData[]
- * }} DrawingData
- *
- */
-function DB () {
-  const firebaseConfig = {
-    apiKey: "AIzaSyDHIJbOtxWHs-79n04xifJCvkuUEqbxvjQ",
-    authDomain: "doodles-sync.firebaseapp.com",
-    databaseURL: "https://doodles-sync.firebaseio.com",
-    projectId: "doodles-sync",
-    storageBucket: "doodles-sync.appspot.com",
-    messagingSenderId: "601337200849",
-    appId: "1:601337200849:web:bbc677a6cd5fa8200445f0"
-  };
-  firebase.initializeApp(firebaseConfig);
-  const database = firebase.database()
-
-  this.auth = callback => {
-    firebase.auth().onAuthStateChanged(function(user) {
-      if (user) {
-        if(typeof callback === 'function') callback(user)
-      } else {
-        if(typeof callback === 'function') callback(null)
-      }
-    })
-    firebase.auth().signInWithEmailAndPassword('tyminko@gmail.com', 'P!*AS5CqcQrg?Sx[')
-      .catch(function(error) {
-        console.error(`%c AUTH %c error: `, 'background:#ffbb00;color:#000', 'color:#00aaff', error)
-      })
-  }
-
-  this.getLastDrawingNumber = () => {
-    return database.ref(`/connections/${uid}`)
-      .once('value')
-      .then(snapshot => {
-        return (snapshot.val() && snapshot.val().number) || 0;
-      })
-  }
-
-  /**
-   * @param {Doodle[]} doodles
-   */
-  this.saveCurrentState = (doodles) => {
-    const state = doodles.map(doodle => {
-      return doodle.segments.map(segment => {
-        return {
-          spin: segment.angle,
-          x: segment.a.x,
-          y: segment.a.y
-        }
-      })
-    })
-    return database.ref(`/states/${uid}`).set(state)
-  }
-
-  /**
-   * @param {number} w
-   * @param {number} h
-   * @param {number} cols
-   * @param {number} rows
-   * @param {Doodle[]} doodles
-   */
-  this.saveDoodles = (w, h, cols, rows, doodles) => {
-    const data = {
-      number: lastDrawingNumber + 1,
-      width: w,
-      height: h,
-      columns: cols,
-      rows: rows,
-      doodles: doodles.map((doodle, i) => doodleToDoodleData(doodle, i))
+export function auth (callback) {
+  firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+      if (typeof callback === 'function') callback(user)
+    } else {
+      if (typeof callback === 'function') callback(null)
     }
-    return database.ref(`/connections/${uid}`).set(data)
-  }
+  })
+  const ps = (location.hash || '').substring(1)
+  if (!ps) return
+  firebase.auth()
+    .signInWithEmailAndPassword(process.env.VUE_APP_DB_USER, ps)
+    .catch(function (error) {
+      console.error('%c AUTH %c: ', 'background:#ff0000;color:#000', 'color:#00aaff', error.code, error.message)
+    })
+}
 
-  /**
-   * @param {Doodle} doodle
-   * @param {number} index
-   * @return {DoodleData}
-   */
-  function doodleToDoodleData (doodle, index) {
-    return {
-      order: index,
-      probabilitySpinFlip: doodle.angleDirChangeChance,
-      drawingPoints: doodle.toShow,
-      segments: doodle.segments.map((segment, i) => segmentToSegmentData(segment, i))
-    }
-  }
+export function subscribeSettings (id, callback) {
+  settingsRef = db.ref(`/settings`)
+  settingsRef.on('value', (snap) => {
+    if (typeof callback === 'function') callback(snap.val())
+  })
+}
 
-  /**
-   * @param {Segment} segment
-   * @param {number} index
-   * @return {SegmentData}
-   */
-  function segmentToSegmentData (segment, index) {
-    return { length: segment.length }
-  }
+export function subscribeDrawing (id, callback) {
+  // if (drawingRef) drawingRef.off()
+  drawingRef = db.ref(`/connections/${id}`)
+  drawingRef.on('value', (snap) => {
+    if (typeof callback === 'function') callback(snap.val())
+  })
+}
+
+export function subscribeState (id, callback) {
+  // if (stateRef) stateRef.off()
+  stateRef = db.ref(`/states/${id}`)
+  stateRef.on('value', (snap) => {
+    if (typeof callback === 'function') callback(snap.val())
+  })
+}
+
+export function unsubscribe () {
+  if (settingsRef) settingsRef.off()
+  if (drawingRef) drawingRef.off()
+  if (stateRef) stateRef.off()
+  settingsRef = null
+  drawingRef = null
+  stateRef = null
 }
